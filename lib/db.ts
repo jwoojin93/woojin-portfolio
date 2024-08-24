7bee3e09b91bf7a65ff1bc4c202ee2d96bafcf08
woojin <jwoojin93@gmail.com>
2024-08-24 23:10:27 +0900

    노마드 캐럿 클론 복사 중

--- lib/db.ts
+++ lib/db.ts
@@ -3,16 +3,3 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 export default db;
-
-async function test() {
-  const user = await db.user.findMany({
-    where: {
-      username: {
-        contains: "est",
-      },
-    },
-  });
-  console.log(user);
-}
-
-test();
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        roboto: "var(--roboto-text)",
+        rubick: "var(--rubick-text)",
+        metallica: "var(--metallica-text)",
+      },
+      margin: {
+        tomato: "120px",
+      },
+      borderRadius: {
+        "sexy-name": "11.11px",
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
