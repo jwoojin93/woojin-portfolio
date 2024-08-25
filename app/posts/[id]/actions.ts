d19fe2b872e0f821c1bf0741d4daa92a04949ab6
woojin <jwoojin93@gmail.com>
2024-08-25 18:24:03 +0900

    remove settimeout

--- app/posts/[id]/actions.ts
+++ app/posts/[id]/actions.ts
@@ -5,7 +5,7 @@ import getSession from "@/lib/session";
 import { revalidateTag } from "next/cache";
 
 export async function likePost(postId: number) {
-  await new Promise((r) => setTimeout(r, 10000));
+  // await new Promise((r) => setTimeout(r, 10000));
   const session = await getSession();
   try {
     await db.like.create({
@@ -19,7 +19,7 @@ export async function likePost(postId: number) {
 }
 
 export async function dislikePost(postId: number) {
-  await new Promise((r) => setTimeout(r, 10000));
+  // await new Promise((r) => setTimeout(r, 10000));
   try {
     const session = await getSession();
     await db.like.delete({
