3e3b79951f8704a0c1d41e0a2aff9bc97115ec7a
woojin <jwoojin93@gmail.com>
2024-09-03 18:35:39 +0900

    scroll arrow button 수정

--- components/arrow-button.tsx
+++ components/arrow-button.tsx
@@ -22,16 +22,9 @@ export default function ArrowButton() {
   }, [pathname]);
 
   const checkScroll = () => {
-    console.log("Math.ceil(window.scrollY): ", Math.ceil(window.scrollY));
-    console.log("window.innerHeight: ", window.innerHeight);
-    console.log(
-      "Math.ceil(window.scrollY) + window.innerHeight: ",
-      Math.ceil(window.scrollY) + window.innerHeight
-    );
-    console.log("window.innerHeight: ", window.innerHeight);
     const last =
       Math.ceil(window.scrollY) + window.innerHeight ===
-      document.body.offsetHeight;
+      document.body.scrollHeight;
     setIsLast(last);
   };
 
